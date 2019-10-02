82af44a5204738cb6e85aa75b5ef55307762fa8c
tanaka-mio-rocal <miosan0226@gmail.com>
2019-10-02 21:27:58 +0900

    1002_night1_commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,32 @@
 <template>
   <div class="board">
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
-    <div class="cell"></div>
+    <ul v-for="item of items" v-bind:key="item.num" class="cell">
+      <li v-if="item.value === '1'" class="whiteball" />
+      <li v-else-if="item.value === '-1'" class="blackball" />
+      <li v-else />
+    </ul>
   </div>
 </template>
 
 <script>
-export default{
+export default {
+  data () {
+    const tempitems = []
+    for (let i = 0; i < 64; i++) {
+      if (i === 27 || i === 36) {
+        tempitems.push({ num: i, value: '1' })
+      } else if (i === 28 || i === 35) {
+        tempitems.push({ num: i, value: '-1' })
+      } else {
+        tempitems.push({ num: i, value: '0' })
+      }
+    }
+    return {
+      items: tempitems,
+      turn: false
+    }
+  }
 }
-
 </script>
 
 <style>
@@ -24,12 +36,27 @@ export default{
   height: 640px;
   background: #060;
 }
-
 .cell{
   width: 12.5%;
   height: 12.5%;
   border: black solid 1px;
   float:left;
+  list-style: none;
+  padding:0.5%;
+}
+.blackball{
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  border: black solid 1px;
+  background: black;
+}
+.whiteball{
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  border: black solid 1px;
+  background: white;
 }
 
 </style>
